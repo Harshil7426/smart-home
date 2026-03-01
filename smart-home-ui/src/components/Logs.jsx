680dc4d8c87fd6c6bd7c00af2b1dff93793c4171
Harshil7426 <harshilaminijs@gmail.com>
2026-03-01 20:10:24 +0530

    changes

--- smart-home-ui/src/components/Logs.jsx
+++ smart-home-ui/src/components/Logs.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:8000";
+const API = "https://smart-home-4b7r.onrender.com/";
 
 export default function Logs() {
   const [logs, setLogs] = useState([]);
